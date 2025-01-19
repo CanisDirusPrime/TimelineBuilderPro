ccc41a4f4f0bb4bead754602a6d3bb3aba11ea15
CanisDirusPrime <160278975+CanisDirusPrime@users.noreply.github.com>
2025-01-19 15:25:37 -0500

    update

--- obj/Debug/net8.0-windows/TimelineBuilderPro.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/TimelineBuilderPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimelineBuilderPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7508ed4508676c4291bea253625808bfacf5f70f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimelineBuilderPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimelineBuilderPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
